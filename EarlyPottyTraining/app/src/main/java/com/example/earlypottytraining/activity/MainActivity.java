be5cf86733a87e45b2a69cacd89617b9f3938139
Sven <wyee0002@gmail.com>
2019-08-27 20:45:52 +1000

    update

--- EarlyPottyTraining/app/src/main/java/com/example/earlypottytraining/activity/MainActivity.java
+++ EarlyPottyTraining/app/src/main/java/com/example/earlypottytraining/activity/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
 import android.widget.TextView;
