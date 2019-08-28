6527294dd21ebf86b689b9f916875611532a4556
Sven <wyee0002@gmail.com>
2019-08-28 22:36:33 +1000

    update

--- EarlyPottyTraining/app/src/main/java/com/example/earlypottytraining/activity/Methods.java
+++ EarlyPottyTraining/app/src/main/java/com/example/earlypottytraining/activity/Methods.java
@@ -3,11 +3,25 @@ package com.example.earlypottytraining.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.earlypottytraining.R;
 
+import java.util.ArrayList;
+
 public class Methods extends BaseActivity {
+
+
+    private LayoutInflater inflater;
+    private ViewPager viewpager;
+    private ArrayList<View> pageview;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +32,43 @@ public class Methods extends BaseActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_home_black_24dp);
 
+        //viewpager
+        viewpager = (ViewPager)findViewById(R.id.vp_img_list);
+
+        inflater = getLayoutInflater();
+        View view1 = inflater.inflate(R.layout.item01,null);
+        View view2 = inflater.inflate(R.layout.item02,null);
+
+        pageview = new ArrayList<View>();
+        pageview.add(view1);
+        pageview.add(view2);
+
+        PagerAdapter mPagerAdapter = new PagerAdapter() {
+            @Override
+            public int getCount() {
+                return pageview.size();
+            }
+
+            @Override
+            public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+                return view==o;
+            }
+
+            @Override
+            public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+                ((ViewPager) container).removeView(pageview.get(position));
+
+            }
+
+            @NonNull
+            @Override
+            public Object instantiateItem(@NonNull ViewGroup container, int position) {
+                ((ViewPager)container).addView(pageview.get(position));
+                return pageview.get(position);
+
+            }
+        };
+        viewpager.setAdapter(mPagerAdapter);
     }
 
     @Override
--- EarlyPottyTraining/app/src/main/java/com/example/earlypottytraining/fragment_nav/IterationOne.java
+++ EarlyPottyTraining/app/src/main/java/com/example/earlypottytraining/fragment_nav/IterationOne.java
@@ -1,21 +1,22 @@
 package com.example.earlypottytraining.fragment_nav;
 
-import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.example.earlypottytraining.R;
 import com.example.earlypottytraining.activity.MainActivity;
 import com.example.earlypottytraining.activity.MapsActivity;
 import com.example.earlypottytraining.activity.Methods;
+
+import java.util.ArrayList;
 
 
 /**
@@ -30,6 +31,9 @@ public class IterationOne extends Fragment {
 
     Button btn_methods;
     Button btn_maps;
+    private LayoutInflater inflater;
+    private ViewPager viewpager;
+    private ArrayList<View> pageview;
 
 
     @Override
@@ -61,6 +65,47 @@ public class IterationOne extends Fragment {
                 startActivity(intent);
             }
         });
+
+
+        //viewpager
+        viewpager = (ViewPager) view.findViewById(R.id.vp_list);
+
+        inflater = getLayoutInflater();
+        View view1 = inflater.inflate(R.layout.item01, null);
+        View view2 = inflater.inflate(R.layout.item02, null);
+
+        pageview = new ArrayList<View>();
+        pageview.add(view1);
+        pageview.add(view2);
+
+        PagerAdapter mPagerAdapter = new PagerAdapter() {
+            @Override
+            public int getCount() {
+                return pageview.size();
+            }
+
+            @Override
+            public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+                return view == o;
+            }
+
+            @Override
+            public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+                ((ViewPager) container).removeView(pageview.get(position));
+
+            }
+
+            @NonNull
+            @Override
+            public Object instantiateItem(@NonNull ViewGroup container, int position) {
+                ((ViewPager) container).addView(pageview.get(position));
+                return pageview.get(position);
+
+            }
+        };
+        viewpager.setAdapter(mPagerAdapter);
+
+
         return view;
     }
 
